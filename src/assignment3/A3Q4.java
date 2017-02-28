9568a6e95a9c0e46b2957b94854d7eed895fd94b
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-28 10:14:49 -0500

    Commit

--- src/assignment3/A3Q4.java
+++ src/assignment3/A3Q4.java
@@ -23,7 +23,7 @@ public class A3Q4 {
         City kw = new City();
 
         //creating a robot 
-        RobotSE joe = new RobotSE(kw, 1, 1, Direction.EAST);
+        RobotSE joe = new RobotSE(kw, 2, 3, Direction.EAST);
 
         //creating walls  
         new Wall(kw, 1, 1, Direction.WEST);
@@ -39,6 +39,33 @@ public class A3Q4 {
         new Wall(kw, 3, 2, Direction.SOUTH);
         new Wall(kw, 3, 3, Direction.SOUTH);
         new Wall(kw, 3, 4, Direction.SOUTH);
+
+
+        //Move to the edge of box
+        if (joe.frontIsClear()) {
+            joe.move();
+        }
+
+        //skim the wall to find exit
+        while (!joe.frontIsClear()) {
+            joe.turnLeft();
+            //continue moving al9ong the wall
+            while (joe.frontIsClear()) {
+                joe.move();
+                joe.turnRight();
+
+                if (joe.frontIsClear()) {
+                    joe.move();
+                    break;
+                }
+
+                if (!joe.frontIsClear()) {
+                    joe.turnLeft();
+                }
+            }
+
+        }
+
 
     }
 }
